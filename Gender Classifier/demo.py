26ffa121b4ad3ae60065aa68d0cf2f7fd510a958
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-04-22 14:07:23 -0700

    Implemented different models

--- Gender Classifier/demo.py
+++ Gender Classifier/demo.py
@@ -1,4 +1,9 @@
 from sklearn import tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -11,10 +16,22 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 
 
 clf = tree.DecisionTreeClassifier()
-
 clf = clf.fit(X,Y)
-
-
 prediction = clf.predict([[170,70,37]])
 
-print (prediction)
+clf2 = MLPClassifier()
+clf2 = clf2.fit(X,Y)
+prediction2 = clf2.predict([[170,70,37]])
+
+clf3 = KNeighborsClassifier()
+clf3 = clf3.fit(X,Y)
+prediction3 = clf3.predict([[170,70,37]])
+
+clf4 = GaussianNB()
+clf4.fit(X,Y)
+prediction4 = clf4.predict([[170,70,37]])
+
+print ("Decision tree classifer: ",prediction)
+print ("MLPClassifier: ", prediction2)
+print ("KNeighborsClassifier: ",prediction3)
+print ("GaussianNB: ",prediction4)
