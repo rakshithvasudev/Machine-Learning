82c769ff8a639c826886a30f524f2bc563b0e807
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-04-22 12:28:40 -0700

    Male female classifier

---
+++ demo.py
@@ -0,0 +1,20 @@
+from sklearn import MLPClassifier
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+
+prediction = clf.predict([[170,70,37]])
+
+print (prediction)
