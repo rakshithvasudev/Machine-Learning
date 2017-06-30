a7bd694f3ecc9b89abf056a6b08387ccfbf72876
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-30 15:26:22 -0700

    added error rate

--- Gradient Descent from Scratch/Gradient-Descent.py
+++ Gradient Descent from Scratch/Gradient-Descent.py
@@ -1,5 +1,30 @@
 from numpy import *
 
+
+def compute_error_for_given_points(b,m, points, learning_rate):
+    ''' Computes the sum of squares distance from the line to the point '''
+    
+    totalError = 0
+    for i in range(len(points)):
+        x = points[i,0]
+        y = points[0,1]
+        totalError += (y -(m*x+b)) ** 2
+    return totalError/len(points)
+
+def step_gradient(b,m, points, learning_rate):
+    ''' Perform gradient descent'''
+
+
+
+def radient_descent_runner(points, starting_b, starting_m, learning_rate,num_iterations):
+    b = starting_b
+    m = starting_m
+
+    for in in range(num_iterations):
+        b,m = step_gradient(b,m, array(points), learning_rate)
+    return [b,m]    
+
+
 def run():
     points = genfromtxt('data.csv', delimiter=",")    
     
@@ -10,7 +35,9 @@ def run():
     initial_b = 0
     initial_m = 0
     num_iterations = 1000
-    
+    [b,m] = gradient_descent_runner(points,initial_b,initial_m,learning_rate,num_iterations)
+    print(b)
+    print(m)
 
 if __name__ =='__main__':
     run()
