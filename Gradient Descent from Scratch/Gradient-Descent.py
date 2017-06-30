075e1fcf60967065c962467dcd71a74bd620f0c9
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-30 14:44:22 -0700

    added data files and converted to python.py file

--- Gradient Descent from Scratch/Gradient-Descent.py
+++ Gradient Descent from Scratch/Gradient-Descent.py
@@ -2,7 +2,15 @@ from numpy import *
 
 def run():
     points = genfromtxt('data.csv', delimiter=",")    
-    print(points)
+    
+    # hyper-parameters
+    learning_rate = 0.0001
+
+    # y = mx + b -> slope of a line
+    initial_b = 0
+    initial_m = 0
+    num_iterations = 1000
+    
 
 if __name__ =='__main__':
     run()
