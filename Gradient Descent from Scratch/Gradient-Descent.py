565c994b65b7238b4d45bfb89ad9eb44da9bde28
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-06-30 16:12:44 -0700

    added descent

--- Gradient Descent from Scratch/Gradient-Descent.py
+++ Gradient Descent from Scratch/Gradient-Descent.py
@@ -21,7 +21,9 @@ def step_gradient(b_current,m_current, points, learning_rate):
         y = points[i,1] 
         b_gradient += -(2/N)*(y-((m_current * x)+b_current))
         m_gradient += -(2/N)*x*(y-((m_current * x)+b_current))
-
+    new_b = b_current - (learning_rate  * b_gradient)    
+    new_m = m_current - (learning_rate  * m_gradient)
+    return [new_b,new_m]
 
 def radient_descent_runner(points, starting_b, starting_m, learning_rate,num_iterations):
     b = starting_b
